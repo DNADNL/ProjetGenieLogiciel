a555336470133ec8dc0d27213385b3c0f04d211a
clementbouly <clementbouly@hotmail.fr>
2016-03-29 13:35:43 +0200

    test

--- AddGoalTests.java
+++ AddGoalTests.java
@@ -13,32 +13,32 @@ public class AddGoalTests {
 		fail("Not yet implemented");
 	}
 
-	@Before
-	public void setUpTestGoalCreatedException() throws Exception {
-		try {
-			FU.deleteGoal("NotInDBTest");
-		} catch (Exception e) {}
-
-	}
-
-	@Test
-	public void testGoalCreatedException() throws ObjectCreatedException {
-		try {
-			FU.addGoal("goal_title", "goal_description", "nickname_test");
-			fail("Expected an UserCreatedException to be thrown");
-		} catch (ObjectAlreadyExistsException e) {
-			fail("Not Expected an UserAlreadyExistsException to be thrown");
-		}
-		catch (ObjectCreatedException e) {
-			assert(e.getName().equals("NotInDBTest"));
-		}
-	}
-	
-	@After
-	public void tearDownTestGoalCreatedException() {
-		try {
-			FU.deleteUser("NotInDBTest");
-		} catch (Exception e) {}
-	}
+//	@Before
+//	public void setUpTestGoalCreatedException() throws Exception {
+//		try {
+//			FU.deleteGoal("NotInDBTest");
+//		} catch (Exception e) {}
+//
+//	}
+//
+//	@Test
+//	public void testGoalCreatedException() throws ObjectCreatedException {
+//		try {
+//			FU.addGoal("goal_title", "goal_description", "nickname_test");
+//			fail("Expected an UserCreatedException to be thrown");
+//		} catch (ObjectAlreadyExistsException e) {
+//			fail("Not Expected an UserAlreadyExistsException to be thrown");
+//		}
+//		catch (ObjectCreatedException e) {
+//			assert(e.getName().equals("NotInDBTest"));
+//		}
+//	}
+//	
+//	@After
+//	public void tearDownTestGoalCreatedException() {
+//		try {
+//			FU.deleteUser("NotInDBTest");
+//		} catch (Exception e) {}
+//	}
 	
 }
